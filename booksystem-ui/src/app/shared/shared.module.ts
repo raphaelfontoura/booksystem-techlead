cec667e618353559a440234298457c93f9e718ba
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-29 09:57:27 -0300

    Adjust layout alert box

--- booksystem-ui/src/app/shared/shared.module.ts
+++ booksystem-ui/src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 
 import { AlertaDialogComponent } from './components/alerta-dialog/alerta-dialog.component';
+
 
 
 @NgModule({
@@ -12,6 +14,7 @@ import { AlertaDialogComponent } from './components/alerta-dialog/alerta-dialog.
   imports: [
     CommonModule,
     MatDialogModule,
+    MatButtonModule,
   ],
   exports: [
     AlertaDialogComponent,
